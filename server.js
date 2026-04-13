686eb429283452536d119e2f38fa946b14ad7428
I Nengah Sukra <adexukhra@gmail.com>
2026-04-13 21:55:35 +0800

    remove sqlite fix deploy

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const sqlite3 = require("sqlite3").verbose();
 const cors = require("cors");
 
 const app = express();
@@ -7,57 +6,34 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
-// Database (aman untuk deploy)
-const db = new sqlite3.Database(":memory:");
-
-// Buat tabel
-db.run(`
-CREATE TABLE IF NOT EXISTS warga (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nik TEXT,
-    no_kk TEXT,
-    nama TEXT,
-    alamat TEXT,
-    banjar TEXT,
-    status TEXT,
-    no_hp TEXT
-)
-`);
+// Database sementara (array)
+let warga = [];
+let idCounter = 1;
 
 // Tambah data
 app.post("/tambah", (req, res) => {
-    const { nik, no_kk, nama, alamat, banjar, status, no_hp } = req.body;
-
-    db.run(
-        "INSERT INTO warga (nik, no_kk, nama, alamat, banjar, status, no_hp) VALUES (?, ?, ?, ?, ?, ?, ?)",
-        [nik, no_kk, nama, alamat, banjar, status, no_hp],
-        () => res.send("Data ditambahkan")
-    );
+    const data = { id: idCounter++, ...req.body };
+    warga.push(data);
+    res.send("Data ditambahkan");
 });
 
 // Ambil data
 app.get("/warga", (req, res) => {
-    db.all("SELECT * FROM warga", [], (err, rows) => {
-        res.json(rows);
-    });
+    res.json(warga);
 });
 
 // Hapus data
 app.delete("/hapus/:id", (req, res) => {
-    db.run("DELETE FROM warga WHERE id=?", [req.params.id], () => {
-        res.send("Data dihapus");
-    });
+    warga = warga.filter(w => w.id != req.params.id);
+    res.send("Data dihapus");
 });
 
 // Edit data
 app.put("/edit/:id", (req, res) => {
-    const { nik, no_kk, nama, alamat, banjar, status, no_hp } = req.body;
-
-    db.run(
-        "UPDATE warga SET nik=?, no_kk=?, nama=?, alamat=?, banjar=?, status=?, no_hp=? WHERE id=?",
-        [nik, no_kk, nama, alamat, banjar, status, no_hp, req.params.id],
-        () => res.send("Data diupdate")
+    warga = warga.map(w =>
+        w.id == req.params.id ? { ...w, ...req.body } : w
     );
+    res.send("Data diupdate");
 });
 
 // Jalankan server
