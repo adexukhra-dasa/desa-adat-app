2534ae81bacc657d30f43639e892b56fdd43afc1
I Nengah Sukra <adexukhra@gmail.com>
2026-04-15 15:58:52 +0800

    fix sqlite ke mongodb

--- server.js
+++ server.js
@@ -1,55 +1,53 @@
 const express = require("express");
-const sqlite3 = require("sqlite3").verbose();
+const mongoose = require("mongoose");
 const cors = require("cors");
-const path = require("path");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
-const db = new sqlite3.Database("./database.db");
+// 🔥 GANTI DENGAN URL MONGODB KAMU
+const MONGO_URL = mongodb+srv://adexukhra_db_user:%40Bali2025@cluster0.cepyhwc.mongodb.net/?appName=Cluster0;
 
-// CREATE TABLE
-db.run(`
-CREATE TABLE IF NOT EXISTS warga (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    no_kk TEXT,
-    nama TEXT,
-    status TEXT,
-    jenis TEXT,
-    gender TEXT
-)
-`);
+// CONNECT
+mongoose.connect(MONGO_URL)
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.log(err));
+
+// SCHEMA
+const Warga = mongoose.model("Warga", {
+    no_kk: String,
+    nama: String,
+    status: String,
+    jenis: String,
+    gender: String
+});
 
 // TAMBAH
-app.post("/tambah", (req, res) => {
-    const { no_kk, nama, status, jenis, gender } = req.body;
-
-    db.run(
-        "INSERT INTO warga (no_kk, nama, status, jenis, gender) VALUES (?, ?, ?, ?, ?)",
-        [no_kk, nama, status, jenis, gender],
-        () => res.send("OK")
-    );
+app.post("/tambah", async (req, res) => {
+    await new Warga(req.body).save();
+    res.send("OK");
 });
 
 // AMBIL
-app.get("/warga", (req, res) => {
-    db.all("SELECT * FROM warga", [], (err, rows) => {
-        res.json(rows);
-    });
+app.get("/warga", async (req, res) => {
+    const data = await Warga.find();
+
+    const hasil = data.map(d => ({
+        id: d._id.toString(),
+        ...d._doc
+    }));
+
+    res.json(hasil);
 });
 
 // HAPUS
-app.delete("/hapus/:id", (req, res) => {
-    db.run("DELETE FROM warga WHERE id=?", [req.params.id], () => {
-        res.send("OK");
-    });
+app.delete("/hapus/:id", async (req, res) => {
+    await Warga.findByIdAndDelete(req.params.id);
+    res.send("OK");
 });
 
 // SERVER
 const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log("Server jalan di port " + PORT);
-});
+app.listen(PORT, () => console.log("Server jalan"));
